10782226f9b64dafb223f7994006f92408467289
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-07-14 19:01:56 +0200

    + keep consistency of settings origin

--- src/config/mongodb_config.rs
+++ src/config/mongodb_config.rs
@@ -14,17 +14,7 @@ impl MongoDbConfig {
     }
 }
 
-pub async fn try_generate_config() -> Result<MongoDbConfig, Error> {
-    let uri: String = match std::env::var(MONGODB_URI) {
-        Ok(uri) => uri,
-        Err(error) => {
-            return Err(Error::new(
-                ErrorKind::AutoConfigFailure,
-                format!("failed to read 'mongodb_uri': {}", error),
-            ));
-        }
-    };
-
+pub async fn try_generate_config(uri: String) -> Result<MongoDbConfig, Error> {
     let client_options = match ClientOptions::parse(uri).await {
         Ok(client_options) => client_options,
         Err(error) => {
